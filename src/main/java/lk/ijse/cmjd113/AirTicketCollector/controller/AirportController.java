c8e40d191cf8924a059594996c2e445d1df100ad
Sathira Perera <sdspe@ou.ac.lk>
2026-01-18 10:13:53 +0530

    Add health check controller

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
@@ -1,4 +1,10 @@
 package lk.ijse.cmjd113.AirTicketCollector.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/airports")
 public class AirportController {
+
 }
