e28420975dc65de48ab108f8d14d60712e16811c
Sathira Perera <sdspe@ou.ac.lk>
2026-01-18 13:04:34 +0530

    Complete Airport Controller

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
@@ -5,10 +5,10 @@ import lk.ijse.cmjd113.AirTicketCollector.util.IDGenerate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/airports")
@@ -18,5 +18,76 @@ public class AirportController {
 public ResponseEntity<AirportDTO> saveAirport(@RequestBody AirportDTO airportDTO){
         airportDTO.setAirportId(IDGenerate.airportId());
     return new ResponseEntity<>(airportDTO, HttpStatus.CREATED);
+}
+
+@GetMapping("/{id}")
+public ResponseEntity<AirportDTO> getSelectedAirport(@PathVariable ("id") String id){
+       var airportDTO =  new AirportDTO(id,
+                "CMB",
+                "Bandaranaike International Airport",
+                "Colombo",
+                "Sri Lanka");
+
+        return new ResponseEntity<>(airportDTO, HttpStatus.OK);
+}
+
+@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<AirportDTO>> getAllAirports(){
+    List<AirportDTO> airports = List.of(
+            new AirportDTO(
+                    "AIR-001",
+                    "CMB",
+                    "Bandaranaike International Airport",
+                    "Colombo",
+                    "Sri Lanka"
+            ),
+            new AirportDTO(
+                    "AIR-002",
+                    "LHR",
+                    "Heathrow Airport",
+                    "London",
+                    "United Kingdom"
+            ),
+            new AirportDTO(
+                    "AIR-003",
+                    "JFK",
+                    "John F. Kennedy International Airport",
+                    "New York",
+                    "United States"
+            ),
+            new AirportDTO(
+                    "AIR-004",
+                    "DXB",
+                    "Dubai International Airport",
+                    "Dubai",
+                    "United Arab Emirates"
+            ),
+            new AirportDTO(
+                    "AIR-005",
+                    "HND",
+                    "Haneda Airport",
+                    "Tokyo",
+                    "Japan"
+            )
+    );
+return  new ResponseEntity<>(airports, HttpStatus.OK);
+}
+
+@DeleteMapping("/{id}")
+public ResponseEntity<Void> deleteAirport(@PathVariable ("id") String id){
+        System.out.println("Delete Airport "+id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+}
+
+@PatchMapping(value = "/{id}", consumes =  MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity<Void> updateAirports(
+        @RequestBody AirportDTO updatedAirport,
+        @PathVariable ("id") String id
+){
+        updatedAirport.setAirportId(id);
+        System.out.println("Update Airport ID "+id);
+        System.out.println("Updated Airport "+updatedAirport);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 }
 }
